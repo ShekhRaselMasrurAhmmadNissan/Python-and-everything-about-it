a7d5c67d5f7cdd63ffe76b0944167dbadfc0e236
ShekhRaselMasrurAhmmadNissan <shekhraselmasrurahmmadnissan@gmail.com>
2020-12-25 10:16:24 +0600

    In Operators folder, created a new file named Logical Operators..

---
+++ Operators/logical_Operators.py
@@ -0,0 +1,12 @@
+name = 'Rasel'
+age = 22
+
+name_matched = name == 'Rasel'
+age_matched = age >= 20
+
+# print(name_matched)
+# print(age_matched)
+
+print(name_matched and age_matched)  # And Operator
+print(name_matched or age_matched)  # Or Operator
+print(not name_matched)  # Not Operator
