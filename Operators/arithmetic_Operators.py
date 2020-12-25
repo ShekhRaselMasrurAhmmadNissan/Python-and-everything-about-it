50b82ab2e9321666986b0446a6455ed3c3c90f7b
ShekhRaselMasrurAhmmadNissan <shekhraselmasrurahmmadnissan@gmail.com>
2020-12-25 09:50:07 +0600

    In Operators Folder, Added a new file called arithmetic Operators..

---
+++ Operators/arithmetic_Operators.py
@@ -0,0 +1,16 @@
+# What is operators..??
+
+# Operators are special symbol which are used to perform different calculation..
+
+a, b = 100, 50
+
+
+# Arithmetic Operators
+
+print(a + b)  # Addition
+print(a - b)  # substract
+print(a * b)  # Multiplication
+print(a / b)  # Division
+print(a // b)  # Floor Division
+print(a % b)  # Reminder
+print(5 ** 3)  # To-The-Power
