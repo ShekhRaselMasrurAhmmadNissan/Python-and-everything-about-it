e6ba5cef33ace4bba9fb3688c58c15aea31a8c9f
ShekhRaselMasrurAhmmadNissan <shekhraselmasrurahmmadnissan@gmail.com>
2021-01-11 20:28:19 +0600

    In the folder 'Control Structure' added a file named 'While'...

---
+++ Control Structure/while.py
@@ -0,0 +1,13 @@
+response = 'y'
+
+while response == 'y':
+    a = int(input('A = '))
+    b = int(input('B = '))
+
+    print(f'Result = {a+b}')
+    response = input('Do you want to continue? (y/n) ')
+
+# n = 1
+# while n <= 100:
+#     print(n, end=' ')
+#     n += 1
