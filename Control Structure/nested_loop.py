17ed7f87ad7dc274636f97c8b7a06c5453fb815f
ShekhRaselMasrurAhmmadNissan <shekhraselmasrurahmmadnissan@gmail.com>
2021-01-11 21:40:29 +0600

    Added a file named 'Nested Loop' in folder 'Control Structure'...

---
+++ Control Structure/nested_loop.py
@@ -0,0 +1,9 @@
+# for number in range(10):
+#     for i in range(5):
+#         print(i, end=' ')
+#     print('')
+
+for i in range(5):
+    for j in range(i + 1):
+        print(j, end='\t')
+    print()
