8d75351cf1363f289bac2c6be268c66b06592892
ShekhRaselMasrurAhmmadNissan <shekhraselmasrurahmmadnissan@gmail.com>
2021-01-11 20:04:06 +0600

    Make a new folder named 'Control Structure'... Add a new file named 'Conditions'...

---
+++ Control Structure/conditions.py
@@ -0,0 +1,23 @@
+bakul = 25
+mukul = 24
+
+print(bakul > mukul)
+
+if (bakul > mukul):
+    print('Bakul is older than Mukul...')
+
+if (mukul < bakul):
+    print('Mukul is younger than Bakul')
+
+if (bakul > mukul):
+    print('Bakul is older than Mukul')
+else:
+    print('Mukul is younger than Bakul...')
+
+a, b = 100, 50
+if (a > b):
+    print('A is Greater...')
+elif (b > a):
+    print('B is Greater...')
+else:
+    print('Both are Same...')
